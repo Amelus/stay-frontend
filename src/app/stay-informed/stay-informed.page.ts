dd621b1562cb4d74b7f3861fe2a5eaccd1248714
Hearues Zueke <hziko314@gmail.com>
2021-09-07 16:25:15 +0200

    edit is working so far, no functions needed for edit

--- src/app/stay-informed/stay-informed.page.ts
+++ src/app/stay-informed/stay-informed.page.ts
@@ -1,5 +1,7 @@
 // @ts-ignore
 import {Component, OnInit} from '@angular/core';
+import {UserVm} from "../api/user/UserVm";
+import {UserClient} from "../api/user/UserClient";
 
 @Component({
   selector: 'app-stay-informed',
@@ -7,10 +9,39 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./stay-informed.page.scss'],
 })
 export class StayInformedPage implements OnInit {
+  currentUser: UserVm;
+  isAdmin: boolean;
+  isEdit: boolean;
 
-  constructor() { }
+  colorEditButtonOff: string;
+  colorEditButtonOn: string;
+  currentEditColor: string;
 
-  ngOnInit() {
+  constructor(private userClient: UserClient) {
+    this.currentUser = this.userClient.getSessionUser();
+    this.isAdmin = this.currentUser.username === 'admin1'; // TODO: need to be moved somewhere else
+    this.isEdit = false;
+
+    this.colorEditButtonOff = '#00FF00';
+    this.colorEditButtonOn = '#FF0000';
+    this.currentEditColor = this.colorEditButtonOn;
   }
 
+  ngOnInit() {
+    this.isEdit = false;
+  }
+
+  private toggleEdit(event: Event) {
+    this.isEdit = !this.isEdit;
+    // const x = document.getElementById('editButton').children[0];   // Get the element with id="demo"
+    if (this.isEdit) {
+      this.currentEditColor = this.colorEditButtonOn;
+    } else {
+      this.currentEditColor = this.colorEditButtonOff;
+    }
+  }
+
+  private addNewCard() {
+    alert('TODO: need to implement this too');
+  }
 }
